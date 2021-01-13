1f8c3da204a8bdd922084e77e0b05bf559620775
Michael <micjanic20@gmail.com>
2021-01-12 23:46:00 -0600

    set up fet and type for fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+// Styles
+import { Wrapper } from './styles/App.styles';
 
-function App() {
+type BitcoinData = {
+  '15m': number;
+  buy: number;
+  last: number;
+  sell: number;
+  symbol: string;
+}
+
+const getBCData = async () => {
+  await (await fetch('https://blockchain.info/ticker')).json();
+}
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">start
     </div>
   );
 }
